88f6e3e78a8c5cb6be45ede41eacf0e11354a294
Harald <haraldcarlsten@yahoo.se>
2023-01-01 13:00:11 +0100

    adds findPeopleByName method to myApp

--- myApp.js
+++ myApp.js
@@ -40,7 +40,12 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-	done(null /*, data*/);
+	Person.find({ name: personName }, function (err, data) {
+		if (err) {
+			return console.error(err);
+		}
+		return done(null, data);
+	});
 };
 
 const findOneByFood = (food, done) => {
